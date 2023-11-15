9c2ccb3ea3639ab1b47f6a617f7a9e331b861bb4
c_u_l8er_true ucimecu inomauto <travis.burandt@gmail.com>
2023-11-15 00:45:23 -0600

    auth

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,7 +1,31 @@
 <script lang="ts">
-  import Login from "$lib/auth/Login.svelte";
+  import { onMount } from "svelte";
+
+  import { AuthAccountLogin } from "subvind-components"
+
+	let organization: any;
+
+  onMount(async () => {
+    let deskHostname = window.location.hostname
+    if (deskHostname === 'localhost') {
+      deskHostname = 'client-area.subvind.com'
+    }
+    const response = await fetch(`https://api.subvind.com/organizations/deskHostname/${deskHostname}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+
+    if (response.ok) {
+      organization = await response.json();
+    } else {
+      const errorData = await response.json();
+      alert(errorData.error);
+    }
+  })
 </script>
 
 <br />
 <br />
-<Login />
+<AuthAccountLogin organization={organization} />
--- src/routes/auth/register/+page.svelte
+++ src/routes/auth/register/+page.svelte
@@ -1,7 +1,31 @@
 <script lang="ts">
-  import Register from "$lib/auth/Register.svelte";
+  import { onMount } from "svelte";
+
+  import { AuthAccountRegister } from "subvind-components"
+
+	let organization: any;
+
+  onMount(async () => {
+    let deskHostname = window.location.hostname
+    if (deskHostname === 'localhost') {
+      deskHostname = 'client-area.subvind.com'
+    }
+    const response = await fetch(`https://api.subvind.com/organizations/deskHostname/${deskHostname}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+
+    if (response.ok) {
+      organization = await response.json();
+    } else {
+      const errorData = await response.json();
+      alert(errorData.error);
+    }
+  })
 </script>
 
 <br />
 <br />
-<Register />
+<AuthAccountRegister organization={organization} />
